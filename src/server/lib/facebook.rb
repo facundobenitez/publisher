252814cc632d03469e8fdffa01f6e93177c26a65
Facundo Benitez <facundo@kelsus.com>
2016-10-09 13:18:43 -0300

    Removed filtering word

--- src/server/lib/facebook.rb
+++ src/server/lib/facebook.rb
@@ -1,29 +1,5 @@
 require 'pry-byebug'
 class Facebook
-
- # def self.publish(post_id, groups)
- #   begin
- #     Koala.config.api_version = "v2.0"
- #     user_graph = Koala::Facebook::API.new(self.oauth_access_token)
- #     group_ids = groups.split(',')
- #     post = Post.find_by(id: post_id)
- #     message = post.body
- #     image_path = post.image.path(:large)
- #     image_content_type = post[:image_content_type]
- #     puts "PUBLISHING GROUP_ID #{group_ids}"
- #     puts "PUBLISHING POST #{message}"
- #     group_ids.each do |group_id|
- #       user_graph.put_picture(
- #         image_path,
- #         image_content_type,
- #         {:message => message},
- #         group_id
- #       )
- #     end
- #   rescue => error
- #     puts error.message
- #   end
- # end
   def self.publish(post_id, groups)
     begin
       group_ids = groups.split(',')
@@ -49,13 +25,13 @@ class Facebook
   def self.groups(group_filtering_words)
     graph = Koala::Facebook::API.new(self.oauth_access_token)
     groups = graph.get_connections("me", "groups")
-    unless group_filtering_words.nil?
-      groups = groups.select do |group|
-        group_filtering_words.any? do |word|
-          group["name"].downcase.include? word
-        end
-      end
-    end
+    #unless group_filtering_words.nil?
+    #  groups = groups.select do |group|
+    #    group_filtering_words.any? do |word|
+    #      group["name"].downcase.include? word
+    #    end
+    #  end
+    #end
     groups
   end
   def self.facebook_app_id
