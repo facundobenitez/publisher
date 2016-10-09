768ba497e56583aa4ca2806610c1543600dd6deb
Facundo Benitez <facundo@kelsus.com>
2016-10-09 11:44:35 -0300

    restored publishing with user id

--- src/server/lib/facebook.rb
+++ src/server/lib/facebook.rb
@@ -1,35 +1,10 @@
 require 'pry-byebug'
 class Facebook
 
-  def self.publish(post_id, groups)
-    begin
-      Koala.config.api_version = "v2.0"
-      binding.pry
-      page_id = '1084851288260830'
-      user_graph = Koala::Facebook::API.new(self.oauth_access_token)
-      page_token = user_graph.get_page_access_token(page_id)
-      page_graph = Koala::Facebook::API.new(page_token)
-      group_ids = groups.split(',')
-      post = Post.find_by(id: post_id)
-      message = post.body
-      image_path = post.image.path(:large)
-      image_content_type = post[:image_content_type]
-      puts "PUBLISHING GROUP_ID #{group_ids}"
-      puts "PUBLISHING POST #{message}"
-      group_ids.each do |group_id|
-        page_graph.put_picture(
-          image_path,
-          image_content_type,
-          {:message => message},
-          group_id
-        )
-      end
-    rescue => error
-      puts error.message
-    end
-  end
  # def self.publish(post_id, groups)
  #   begin
+ #     Koala.config.api_version = "v2.0"
+ #     user_graph = Koala::Facebook::API.new(self.oauth_access_token)
  #     group_ids = groups.split(',')
  #     post = Post.find_by(id: post_id)
  #     message = post.body
@@ -37,9 +12,8 @@ class Facebook
  #     image_content_type = post[:image_content_type]
  #     puts "PUBLISHING GROUP_ID #{group_ids}"
  #     puts "PUBLISHING POST #{message}"
- #     graph = Koala::Facebook::API.new(self.oauth_access_token)
  #     group_ids.each do |group_id|
- #       graph.put_picture(
+ #       user_graph.put_picture(
  #         image_path,
  #         image_content_type,
  #         {:message => message},
@@ -50,6 +24,28 @@ class Facebook
  #     puts error.message
  #   end
  # end
+  def self.publish(post_id, groups)
+    begin
+      group_ids = groups.split(',')
+      post = Post.find_by(id: post_id)
+      message = post.body
+      image_path = post.image.path(:large)
+      image_content_type = post[:image_content_type]
+      puts "PUBLISHING GROUP_ID #{group_ids}"
+      puts "PUBLISHING POST #{message}"
+      graph = Koala::Facebook::API.new(self.oauth_access_token)
+      group_ids.each do |group_id|
+        graph.put_picture(
+          image_path,
+          image_content_type,
+          {:message => message},
+          group_id
+        )
+      end
+    rescue => error
+      puts error.message
+    end
+  end
   def self.groups(group_filtering_words)
     graph = Koala::Facebook::API.new(self.oauth_access_token)
     groups = graph.get_connections("me", "groups")
